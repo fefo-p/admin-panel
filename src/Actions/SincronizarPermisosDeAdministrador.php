26100df8a70d8f9701225a5dc4eb253d3ec05c2b
Fernando Pintabona <fernando.pintabona@pjn.gov.ar>
2024-05-10 15:28:58 -0300

    Sincronizar permisos de administrador

--- src/Actions/SincronizarPermisosDeAdministrador.php
+++ src/Actions/SincronizarPermisosDeAdministrador.php
@@ -2,6 +2,7 @@
 
     namespace FefoP\AdminPanel\Actions;
 
+    use Illuminate\Support\Str;
     use FefoP\AdminPanel\Models\Role;
     use FefoP\AdminPanel\Models\Permission;
     use Spatie\Permission\PermissionRegistrar;
@@ -10,8 +11,13 @@
     {
         public function __invoke(): void
         {
-            $rol = Role::where('name', config('adminpanel.admin.role_name'))->first();
-            $permisos = Permission::where('guard_name', 'web')->pluck('id');
+            $rol      = Role::where('name', config('adminpanel.admin.role_name'))->first();
+            $permisos = Permission::where('guard_name', 'web')
+                                  ->get()
+                                  ->reject(function ($permission) {
+                                      return Str::of($permission->name)->startsWith('adminpanel') && ! Str::of($permission->name)->endsWith('.ver');
+                                  })
+                                  ->pluck('id');
             $rol->syncPermissions($permisos);
 
             app()->make(PermissionRegistrar::class)->forgetCachedPermissions();
